d9f0810d7a3ae8f5961999773d1d79b6d3a2e908
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-19 15:54:53 -0600

    Used style-components to further style cards

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Card, Icon } from 'semantic-ui-react'
-import { Cards } from './Styling'
+import { Cards, Header, SubHeader, Description, Left, Right } from './Styling'
 
 export default function CharacterCard(props) {
   console.log(props);
@@ -13,13 +13,33 @@ export default function CharacterCard(props) {
     //   </Card.Content>
     // </Card>
     <Cards>
+      <Header>
         {props.name}
-       {props.gender}
+      </Header>
+      <SubHeader>
         {props.birth}
-         {props.height}
-        {props.mass}
-         {props.hair}
-        {props.eyes}
+      </SubHeader>
+      <h3>Description</h3>
+      <Description>
+        <Left>
+          <p>Gender:</p>
+          <p>Hair Color:</p> 
+          <p>Eye Color:</p> 
+          <p>Height:</p> 
+          <p>Mass:</p>  
+        </Left>
+        <Right>
+          <p>{props.gender}</p>
+          <p>{props.hair}</p>
+          <p>{props.eyes}</p>
+          <p>{props.height}</p>
+          <p>{props.mass}</p>
+          
+        </Right>
+        
+      </Description>
+        
+       
 
     </Cards>
   );
--- starwars/src/components/Styling.js
+++ starwars/src/components/Styling.js
@@ -4,23 +4,56 @@ export const Main = styled.main`
     margin: 0 auto;
     border:1px solid red;
     max-width:100%;
-    width: 1000px;
+    width: 1200px;
+    display:flex;
+    flex-direction: column;
+    flex: wrap;
+    justify-content:center;
 `;
 
 export const CardContainer = styled.div`
-    /* margin: 0 auto; */
+    margin: 0 auto;
     border: 1px solid green;
     max-width:100%;
-    width: 900px;
+    width: 1000px;
+    /* display:flex;
+    flex: wrap;
+    justify-content:flex-start; */
 `;
 
 export const Cards = styled.div`
-    /* display:flex;
-    flex: wrap;
-    flex-direction: column; */
+    display:flex;
+    flex-direction: column;
+    border: 1px solid blue;
     background: white;
     max-width:100%;
     width: 300px;
-    height:300px;
-    margin: 20px;
+    margin: 10px;
+`;
+
+export const Header = styled.header`
+    font-size: 26px;
+    font-weight: 600;
+    margin: 30px 0 0;
+`;
+
+export const SubHeader = styled.h2`
+    font-size: 20px;
+    font-weight: 500;
+`;
+
+export const Description = styled.div`
+    display:flex;
+    /* justify-content: space-around; */
+    padding: 10px;
+`;
+
+export const Left = styled.div`
+    width:50%;
+    border: 1px solid purple;
+`;
+
+export const Right = styled.div`
+    width:50%;
+    border: 1px solid hotpink;
 `;
