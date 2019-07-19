8c757029e0e0391f8113b84be57a46ea03a5cd63
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-19 13:29:50 -0600

    Debbuged issues with terminal and cleaned out unneeded files

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+// import { Card } from 'semantic-ui-react';
 
 export default function CharacterCard(props) {
     console.log(props);
